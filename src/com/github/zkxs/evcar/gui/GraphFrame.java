dbf6e7e361b9a294c72469c5396f8a2611a94d30
Michael Ripley <zkxs00@gmail.com>
2016-04-25 03:27:37 -0500

    Added a keybinding to toggle fullscreen
    Note that it doesn't actually do anything yet

--- src/com/github/zkxs/evcar/gui/GraphFrame.java
+++ src/com/github/zkxs/evcar/gui/GraphFrame.java
@@ -1,11 +1,16 @@
 package com.github.zkxs.evcar.gui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 import javax.swing.border.EmptyBorder;
 
 import com.github.zkxs.evcar.Driver;
@@ -39,6 +44,17 @@ public class GraphFrame extends JFrame
 		Histogram currentHistogram = new Histogram(GaugeParameters.CURRENT_GAGUE_PARAMETERS);
 		contentPane.add(currentHistogram, BorderLayout.CENTER);
 		
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_F11, 0), "toggleFullscreen");
+		getRootPane().getActionMap().put("toggleFullscreen", new AbstractAction() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				toggleFullscreen();
+			}
+		});
+		
 		dataProvider.addListener(currentHistogram);
 		
 		Timer updateTimer = new Timer("Update Timer");
@@ -49,6 +65,11 @@ public class GraphFrame extends JFrame
 				dataProvider.dispatchData();
 			}
 		}, 0, 1000);
+	}
+	
+	private void toggleFullscreen()
+	{
+		System.out.println("hey");
 	}
 	
 }
