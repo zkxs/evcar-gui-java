65c1bcbd6d142b6d65a0fa3721bbeb7633f48fe1
Michael Ripley <zkxs00@gmail.com>
2016-04-24 22:23:26 -0500

    Created concept of a DataPoint

---
+++ src/com/github/zkxs/evcar/data/DataPoint.java
@@ -0,0 +1,50 @@
+package com.github.zkxs.evcar.data;
+
+public class DataPoint
+{	
+	private long time;
+	private double current;
+	private double voltage;
+	private double rpm;
+	
+	
+	public long getTime()
+	{
+		return time;
+	}
+	
+	void setTime(long time)
+	{
+		this.time = time;
+	}
+	
+	public double getCurrent()
+	{
+		return current;
+	}
+	
+	void setCurrent(double current)
+	{
+		this.current = current;
+	}
+	
+	public double getVoltage()
+	{
+		return voltage;
+	}
+	
+	void setVoltage(double voltage)
+	{
+		this.voltage = voltage;
+	}
+	
+	public double getRpm()
+	{
+		return rpm;
+	}
+	
+	void setRpm(double rpm)
+	{
+		this.rpm = rpm;
+	}
+}
--- src/com/github/zkxs/evcar/data/DataProvider.java
+++ src/com/github/zkxs/evcar/data/DataProvider.java
@@ -2,5 +2,20 @@ package com.github.zkxs.evcar.data;
 
 public interface DataProvider
 {	
+	public void start();
+	public void restart();
+	public void stop();
 	
+	/**
+	 * Constructs a new DataPoint object
+	 * @return Most recent data from provider
+	 */
+	public DataPoint getDataPoint();
+	
+	/**
+	 * Reuses an old DataPoint object
+	 * @param unusedDataPoint the DataPoint to recycle
+	 * @return Most recent data from provider
+	 */
+	public DataPoint getDataPoint(DataPoint unusedDataPoint);
 }
